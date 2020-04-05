2a4c7a0ed52753eed523b1bfdb67443a3bdfb142
Bernat <bernatesquirol@gmail.com>
2020-04-05 09:55:58 +0200

    scope

--- src/Utils.js
+++ src/Utils.js
@@ -2,12 +2,13 @@ import firebase from 'firebase'
 
 export const changeDynamicManifest = (name, fav, path)=>{  
     var myDynamicManifest = {
-      "name": "Comunit.app "+name,
+      "name": name,
       "short_name": name,
-      "start_url": 'https://www.comunit.app/'+path,
+      "start_url": ".",
       "display": "standalone",
       "theme_color": "#000000",
       "background_color": "#ffffff",
+      "scope": "https://comunit.app/"+path,
       "icons": [{
         "src": fav+"/favicon.ico",
         "sizes": "64x64 32x32 24x24 16x16",
